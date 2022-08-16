f1ff06d13815b5b67bcd9c4408016f0089cb1f2a
TienTran-commits <tientran99.work@gmail.com>
2022-08-16 14:57:15 +0700

    Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    redirect_to root_path, flash[:danger] = t("error.usr_not_found")
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t "home.welcome_message"
+      redirect_to @user
+    else
+      flash[:danger] = t "error.signup_failed"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
