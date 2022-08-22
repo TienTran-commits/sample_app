ec321fcdfd0b7a9ddcbf345a02f63ca78d27c63c
TienTran-commits <tientran99.work@gmail.com>
2022-08-22 11:33:08 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save {email.downcase!}
   validates :name, presence: true,
                    length: {maximum: Settings.validation.name_max_digits}
@@ -10,7 +12,7 @@ class User < ApplicationRecord
                       length: {minimum: Settings.validation.password_min_digits}
   has_secure_password
 
-  # Return the hash digest of the given string
+  # Return the hash digest of the given string.
   class << self
     def digest string
       cost = if ActiveModel::SecurePassword.min_cost
@@ -18,7 +20,30 @@ class User < ApplicationRecord
              else
                BCrypt::Engine.cost
              end
-      BCrypt::Password.create(string, cost)
+      BCrypt::Password.create(string, cost:)
     end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the db for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
